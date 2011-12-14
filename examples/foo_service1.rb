2da7559d06f9af7c3c0e11944a4a55677b2f38dc
pachacamac <pachacamac@lavabit.com>
2011-12-14 17:22:38 +0100

    waiting for middleware to be ready before registering at zookeeper

--- examples/foo_service1.rb
+++ examples/foo_service1.rb
@@ -2,8 +2,16 @@ require 'rubygems'
 require 'sinatra'
 require File.dirname(__FILE__) + '/zk-register'
 
-settings.port = 7777
-zk_register("foo")
+configure do
+  settings.port = 7777
+
+  Thread.new{
+    puts "Waiting for Sinatra to get his pants on..."
+    sleep 1 until Sinatra::Application.running?
+    puts "Registering at Zookeeper server..."
+    zk_register("foo")
+  }
+end
 
 get '/' do
     'I am instance ONE of the FOO SERVICE!'
