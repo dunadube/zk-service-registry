afbbe4a771647e0483c997075f1aa874b0f0d6c2
Pacha Camac <pachacamac@lavabit.com>
2011-12-15 11:38:36 +0100

    better method to find ip - but only for 1.9.2+

--- examples/zk-register.rb
+++ examples/zk-register.rb
@@ -1,9 +1,9 @@
 require 'zk-service-registry'
+require 'socket'
 
 def local_ip
-  require 'socket'
+  #Socket.ip_address_list.find(&:ipv4_private?).ip_address #>=1.9.2-180
   orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true  # turn off reverse DNS resolution temporarily
-
   UDPSocket.open do |s|
     s.connect '64.233.187.99', 1
     s.addr.last
@@ -15,6 +15,5 @@ end
 def zk_register(svcname)
   ipaddr = settings.bind
   ipaddr = local_ip if ipaddr == "0.0.0.0"
-
   ZK::ServiceInstance.advertise(svcname, ipaddr + ":" + settings.port.to_s)
 end
